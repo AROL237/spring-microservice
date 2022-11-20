42b80750e4182a7f03ef0839ca5a970cc1ae26e9
SIGNING AROL <85204727+AROL237@users.noreply.github.com>
2022-11-20 17:03:50 +0100

    - movie catalog microservice put in place,with the movie-catalog-service calling both the movie-info-service and the rating-data-service and getting a response.
    - A discovery-server (eureka) to make service discovery easy and have an idea of the status of each service in the system.

--- Movie-Catalogue-Service/src/main/java/com/signing/moviecatalogueservice/Controller/MovieCatalogController.java
+++ Movie-Catalogue-Service/src/main/java/com/signing/moviecatalogueservice/Controller/MovieCatalogController.java
@@ -3,23 +3,23 @@ package com.signing.moviecatalogueservice.Controller;
 import com.signing.moviecatalogueservice.Entities.CatalogItem;
 import com.signing.moviecatalogueservice.Entities.Movie;
 import com.signing.moviecatalogueservice.Entities.UserRating;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
+@RequiredArgsConstructor
 public class MovieCatalogController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
 //    @Autowired
 //    private WebClient.Builder webClientBuilder;
@@ -36,7 +36,7 @@ public class MovieCatalogController {
          return userRating.getRatingList().stream().map(rating -> {
             Movie movie =restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(),Movie.class);
 
-             //async call using web client
+             //api call with webClient
 //             Movie movie = webClientBuilder.build()
 //                     .get()
 //                     .uri("http://localhost:8081/movies/\"+rating.getMovieId()")
